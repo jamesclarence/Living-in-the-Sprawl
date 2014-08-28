2fb54533017916c8d842431f9398ac23f34a1f7a
Jay Fisher <fisher.jc@gmail.com>
2014-08-28 18:47:04 -0500

    Add plots and updated code

--- Wooster Football/wooster_football.R
+++ Wooster Football/wooster_football.R
@@ -12,15 +12,22 @@ woossn <- read.csv("woo_foot_ssn.csv")
 
 ### What I want to look up:
 # 1. Winning Percentage - each season
+png("plot1.png", height = 350, width = 550)
 ggplot(woossn, aes(x=Season, y=WinPct)) +
     geom_bar(fill="gold",color="black",stat="identity") +
     labs(x="Season", y="Winning Percentage", 
          title="College of Wooster Football, 1999-2013") +
     theme_minimal()
+dev.off()
 
 # 2. Point Differential
+png("plot2.png", height = 350, width = 550)
 ggplot(woossn, aes(x=Wins, y=Differential)) +
-    geom_point(color="gold", aes(size = 4)) +
+    geom_point(color="black", size=6, alpha=8/10) +
+    geom_point(color="gold", size=5) +
     labs(x="Wins", y="Point Differential",
          title="College of Wooster Football Point Differential, 1999-2013") +
+    geom_hline(aes(yintercept=0), color="grey",linetype="dashed") +
+    guides(size=F) +
     theme_minimal()
+dev.off()
