91f24c2c1206ebf5963a6bbea3b60c218b593844
Jay Fisher <fisher.jc@gmail.com>
2014-11-13 22:08:11 -0600

    new plots

--- Lawmakers/law_census.R
+++ Lawmakers/law_census.R
@@ -28,11 +28,18 @@ data$les <- as.numeric(levels(data$les))[data$les] # change les to numbers
 # Democrats: 93-103 (1973-95); 110-111 (2007-2011)
 dem1 <- data.frame(xmin_d1 = factor(1973), xmax_d1 = factor(1995), ymin_d1 = -Inf, ymax_d1 = Inf)
 dem2 <- data.frame(xmin_d2 = factor(2007), xmax_d2 = factor(2009), ymin_d2 = -Inf, ymax_d2 = Inf)
+demc1 <- data.frame(xmin_d1 = factor(93), xmax_d1 = factor(103), ymin_d1 = -Inf, ymax_d1 = Inf)
+demc2 <- data.frame(xmin_d2 = factor(110), xmax_d2 = factor(111), ymin_d2 = -Inf, ymax_d2 = Inf)
 
 # Dem Shade
 rect_d1 <- geom_rect(data = dem1, aes(xmin = xmin_d1, xmax = xmax_d1, ymin = ymin_d1, ymax = ymax_d1), 
                      fill = "blue", alpha=0.25, inherit.aes = FALSE)
 rect_d2 <- geom_rect(data = dem2, aes(xmin = xmin_d2, xmax = xmax_d2, ymin = ymin_d2, ymax = ymax_d2), 
+                     fill = "blue", alpha=0.25, inherit.aes = FALSE)
+
+rect_cd1 <- geom_rect(data = demc1, aes(xmin = xmin_d1, xmax = xmax_d1, ymin = ymin_d1, ymax = ymax_d1), 
+                     fill = "blue", alpha=0.25, inherit.aes = FALSE)
+rect_cd1 <- geom_rect(data = demc2, aes(xmin = xmin_d2, xmax = xmax_d2, ymin = ymin_d2, ymax = ymax_d2), 
                      fill = "blue", alpha=0.25, inherit.aes = FALSE)
 
 # Republicans: 104-109 (1995-2007); 112 (2011-2013)
@@ -49,10 +56,13 @@ rect_r2 <- geom_rect(data = rep2, aes(xmin = xmin_r2, xmax = xmax_r2, ymin = ymi
 grp <- group_by(data, congress, year, majority)
 grp_2 <- group_by(data, congress, year, Region)
 
-## Plot 1: Effectiveness of the Majority Party versus Minority Party, 1973-2011
-grp %>%
-    summarise(majority_n = n(), les_majority = sum(les), ratio = (les_majority/majority_n)) %>%
-    ggplot(aes(x = year, y = ratio, group = majority)) +
+### Part 1
+
+# Part 1, Plot 1: Effectiveness of the Majority Party versus Minority Party, 1973-2011
+plot1part1 <- grp %>%
+    summarise(majority_n = n(), les_majority = sum(les), ratio = (les_majority/majority_n))
+    png("part1_plot1.png", height = 325, width = 550)
+    ggplot(plot1part1, aes(x = year, y = ratio, group = majority)) +
         geom_line() +
         geom_point(aes(shape = majority, size = 2)) +
         scale_y_continuous(breaks = seq(0, 2, 0.5)) +
@@ -66,9 +76,50 @@ grp %>%
               axis.text.y = element_text(face = "bold"),
               axis.title.y = element_text(face = "bold")) +
         labs(x = "", y = "Legislative Effectiveness Score")
-# 550 x 325
+    dev.off()
 
-## Plot 2: Number of members per Region per Congress
+## Part 1, Plot 2: Distribution of Individual LES by Majority & Minority
+png("part1_plot2.png", height = 325, width = 550)
+ggplot(grp, aes(x = congress, y = les, shape = majority, color = majority)) +
+    geom_point(alpha = 2/5) +
+    geom_jitter() +
+    scale_y_sqrt() +
+    scale_shape_discrete(name = "Party",
+                         breaks = c("1","0"),
+                         labels = c("Majority", "Minority")) +
+    scale_color_discrete(name = "Party",
+                         breaks = c("1","0"),
+                         labels = c("Majority", "Minority")) +
+    theme_minimal() + 
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "bold"),
+          axis.text.y = element_text(face = "bold"),
+          axis.title.y = element_text(face = "bold")) +
+    labs(x = "", y = "Legislative Effectiveness Score")
+dev.off()
+
+## Part 1, Plot 3: Nancy Pelosi versus John Boehner
+nancy_john <- filter(join, thomas_name == "Pelosi, Nancy" | thomas_name == "Boehner, John")
+png("part1_plot3.png", height = 325, width = 550)
+ggplot(nancy_john, aes(x = year, y = les, shape = thomas_name, group = thomas_name)) +
+    geom_line() +
+    geom_point(aes(size = 3)) +
+    rect_d1 + rect_r1 + rect_d2 + rect_r2 +
+    scale_shape_discrete(breaks = c("Boehner, John","Pelosi, Nancy"),
+                         labels = c("John Boehner", "Nancy Pelosi")) +
+    theme_minimal() +
+    guides(size = F) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "bold"),
+          axis.text.y = element_text(face = "bold"),
+          axis.title.y = element_text(face = "bold"),
+          legend.title = element_blank(),
+          plot.title = element_text(size = 11.5, face = "bold")) +
+    labs(x = "", y = "Legislative Effectiveness Score",
+         title = "Effectiveness of John Boehner and Nancy Pelosi")
+dev.off()
+
+### Part 2
+
+# Plot 1: Number of members per Region per Congress
 grp_2 %>%
     summarise(members=n()) %>%
     ggplot(aes(x = year, y = members, group = Region, color = Region)) +
@@ -80,7 +131,7 @@ grp_2 %>%
         labs(x = "", y = "", 
               title = "Number of House of Representatives Members Per Region, 1973-2011")
 
-## Plot 3: LES score per region per region's member
+# Plot 2: LES score per region per region's member
 grp_2 %>%
     summarise(members=n(), les_region = sum(les), ratio = les_region/members) %>%
     ggplot(aes(x = year, y = ratio, group = Region, color = Region)) +
@@ -93,9 +144,4 @@ grp_2 %>%
         labs(x = "", y = "LES Score",
              title = "Average Effectiveness Per Representative in Each Region")
 
-# Plots Ideas:
-    # 1. Democrats & Republicans
-    # 2. Divisions within South region
-        # 2a. South as a whole
-        # 2b. Democrat & Republicans
-    # 3. Boehner vs Pelosi
+# Plot 3: In depth look at South's sub-division
