d86439d916eddcba3874c6d6b283288633204b2c
Jay Fisher <fisher.jc@gmail.com>
2015-01-09 19:47:37 -0600

    Final code commit

--- How Slow Am I/howslow.R
+++ How Slow Am I/howslow.R
@@ -1,25 +1,5 @@
 ### How Slow Am I
 
-### Races
-# Athens Half Marathon (2011)
-    # http://results.active.com/events/athens-ga-half-marathon-athhalf2011/males-overall
-# Iron Horse 15K (2011) - can't find results
-# Save the Light Half Marathon (Folly Beach, SC) (2/4/2012)
-    # http://actioncarolina.com/sl12hres.html
-# Olde Rope Mill (4/7/2012)
-    # http://www.athlinks.com/race/event?raceid=200974
-# Red Top Roaster (8/4/2012)
-    # http://www.athlinks.com/Race/Event?raceID=222383&courseID=308130
-# Atlanta Marathon (10/28/2012)
-    # http://atlantatrackclub.org/documents/download/2013amresults_final
-# Tom King Classic (East Nashville Half Marathon) (3/9/2013)
-    # http://www.nashvillestriders.com/storage/results/results-2013/TK%20HALF%20OVERALL%202013.txt
-# RunWILD Peeler Park (4/7/2013) - can't find results
-# 4 Bridges Half Marathon (10/20/2013) - http://jamesclarence.wordpress.com/2013/10/28/4-bridges-half-marathon-in-box-plots/
-    # https://dl.dropboxusercontent.com/u/52479449/Results/4B_ResultsOverall.html
-# Gobble Jog (11/28/2014)
-    # http://results.active.com/events/mdj-must-gobble-jog-5k-10k/10k-award-winners
-
 library(XML)
 library(RCurl)
 library(ggplot2)
@@ -27,56 +7,31 @@ library(dplyr)
 library(data.table)
 library(reshape2)
 
-## 4 Bridges Half Marathon (2013)
+### Data Scraping
+# 4 Bridges Half Marathon (2013)
 url_4b <- "https://dl.dropboxusercontent.com/u/52479449/Results/4B_ResultsOverall.html"
 html_4b <- getURL(url_4b)
 tbl_4b <- readHTMLTable(html_4b, as.data.frame = T, which = 1)
 
-# Rename columns; Extra space at end of column names
-# use data.table to rename
+# Use data.table to rename columns; Extra space at end of column names
 colnames(tbl_4b)
 # [1] "Position "    "Bib # "       "Name "        "Finish Time "
 # [5] "Pace "        "Chip Time "   "Division "    "Gender " 
 
-setnames(tbl_4b, old=c("Position ","Bib # ", "Name ", "Finish Time ", "Pace ","Chip Time ", "Division ", "Gender "), 
-         new=c("Position", "Bib", "Name", "Time_Finish", "Pace", "Time_Chip", "Division", "Gender"))
+setnames(tbl_4b, old=c("Position ","Bib # ", "Name ", "Finish Time ", "Pace ",
+                       "Chip Time ", "Division ", "Gender "), 
+        new=c("Position", "Bib", "Name", "Time_Finish", "Pace", "Time_Chip", 
+               "Division", "Gender"))
 
 write.csv(tbl_4b, "4briges2013.csv", row.names=F)
-# Time format is off in Excel, where I read CSV file, but it is fine when
-# custom formatted in the Time category
+# Time format is off in Excel, but it is fine when custom formatted in the Time 
+# category in Excel
 
 # Read all races (except Iron Horse 15K & RunWild15K) from CSV
 setwd("~/Documents/Blog/Living-in-the-Sprawl/How Slow Am I")
 times <- read.csv("times.csv")
 
 times$hms <- as.POSIXct(as.character(times$TIME), format = "%H:%M:%S")
-
-# All Races Distribution
-ggplot(times, aes(x=hms, fill = as.factor(Distance))) +
-    geom_histogram(binwidth = 30, alpha = 0.5, position = "identity") +
-    theme_classic()
-    
-
-ggplot(times, aes(x=hms, fill = as.factor(Distance))) +
-    geom_density(alpha = .5) +
-    theme_classic()
-        
-
-# Half Marathon Distribution
-filter(times, Distance == 13.1) %>%
-    ggplot(aes(x=hms)) + 
-        geom_histogram()
-
-# Marathon Distribution
-filter(times, Distance == 26.2) %>%
-    ggplot(aes(x=hms)) + 
-    geom_histogram()
-
-
-filter(times, Distance == 6.2) %>%
-    ggplot(aes(x=hms)) + 
-    geom_histogram() +
-    theme_classic()
 
 ### Plot 1: All Races, by Time and Result
 
@@ -89,7 +44,6 @@ breaks_all <- scale_color_discrete(name = "Events",
                                               "Gobble Jog", "Olde Rope", "Red Top",
                                               "Save The Light", "Tom King"))
 
-# All Races
 png("plot1.png", height = 325, width = 575)
 times %>% # Like this one
     group_by(PLACE, JAY, hms) %>%
@@ -106,34 +60,7 @@ times %>% # Like this one
               legend.justification=c(1,0), legend.position=c(1,0))
 dev.off()
 
-# Half Marathon Results
-half <- filter(times, Distance == 13.1)
-
-breaks_half <- scale_color_discrete(name = "Half Marathons",
-                                    breaks = c("4 Bridges", "Athens Half Marathon", "Save the Light", "TomKingClassic"),
-                                    labels = c("4 Bridges",
-                                               "Athens",
-                                               "Save The Light",
-                                               "Tom King"))
-
-half %>%
-    group_by(PLACE, JAY, hms) %>%
-    ggplot(aes(x = hms, y = PLACE)) +
-        geom_point(aes(color=Event, size = JAY)) +
-        guides(size = F) +
-        theme_classic() +
-        theme(axis.text.x = element_text(face = "bold"),
-              axis.title.x = element_text(face = "bold"),
-              axis.text.y = element_text(face = "bold"),
-              axis.title.y = element_text(face = "bold")) +
-        labs(x = "Finish Time", y = "Place") +
-        breaks_half
-
-# Median per Event
-group_by(times, Event) %>%
-    summarise(median = median(hms, na.rm=T)) 
-
-### Plot 2: Percentile Finish
+### Plot 2: Percentile Finish: Comparison with All Ages and Ages 50+
 
 jay_place <- select(times, Event, Year, Distance, JAY, PLACE) %>%
     arrange(Year) %>%
@@ -147,35 +74,15 @@ jay <- inner_join(x = jay_place, y = event_place, by = "Event")
 
 jay_pct <- jay %>%
     mutate(percentile = round(100*(1 - (PLACE/OutOf)),1))
-    
-ggplot(jay_pct, aes(x = Year, y = percentile)) +
-    geom_point(aes(color = Event, size = 10), alpha = .8) +
-    theme_classic() +
-    guides(size = F) +
-    breaks_all +
-    scale_size_continuous(range = range(c(3,10))) +
-    theme(axis.text.x = element_text(face = "bold"),
-          axis.title.x = element_text(face = "bold"),
-          axis.text.y = element_text(face = "bold"),
-          axis.title.y = element_text(face = "bold")) +
-    labs(x = "Year", y = "Percentile Finish")
 
-         
-### Compare myself with different age groups (hilarious)
-    # filter me + ages 50 and over; me + my age and younger
-
-### Plot 3: Compare myself with different age groups
-
-# Ages 50+ and I
+# Reading document again to practice with stringsAsFactor = F
 times2 <- read.csv("times.csv", stringsAsFactor = F)
 
 times2$AGE <- as.numeric(times2$AGE)
 times2$hms <- as.POSIXct(as.character(times2$TIME), format = "%H:%M:%S")
 
-# No ages in 4 Bridges
-
 jay50_data <- filter(times2, AGE >= "50" | JAY == "Y")
-jay50_data <- filter(jay50_data, Event != "4 Bridges")
+jay50_data <- filter(jay50_data, Event != "4 Bridges") # No ages in 4 Bridges
 
 # Need to make new Places based on jay50_data
 jay50_data <- group_by(jay50_data, Event) %>%
@@ -194,18 +101,6 @@ jay50 <- inner_join(x = jay50_place, y = event50_place, by = "Event")
 jay50_pct <- jay50 %>%
     mutate(percentile = round(100*(1 - (place50/OutOf)),1))
 
-ggplot(jay50_pct, aes(x = Year, y = percentile)) +
-    geom_point(aes(color = Event, size = 10), alpha = .8) +
-    theme_classic() +
-    guides(size = F) +
-    breaks_all +
-    scale_size_continuous(range = range(c(3,10))) +
-    theme(axis.text.x = element_text(face = "bold"),
-          axis.title.x = element_text(face = "bold"),
-          axis.text.y = element_text(face = "bold"),
-          axis.title.y = element_text(face = "bold")) +
-    labs(x = "Year", y = "Percentile Finish")
-
 # Combine Normal and Age 50 Comparison Percentiles
 jay_all50 <- left_join(x = jay_pct, y = jay50_pct, by = "Event")
 
@@ -218,6 +113,84 @@ jay_all50 <- left_join(x = jay_pct, y = jay50_pct, by = "Event")
 jay_all50 <- melt(jay_all50, id.vars = c(1:6, 8:13), variable.name = "filter", 
                   value.name = "percentile")
 
+# Facets
+jay_all50_2 <- jay_all50
+
+levels(jay_all50_2$filter)[levels(jay_all50_2$filter)=="percentile.x"] <- "All Ages"
+levels(jay_all50_2$filter)[levels(jay_all50_2$filter)=="percentile.y"]   <- "Ages 50+"
+
+png("plot2.png", height = 325, width = 575)
+ggplot(jay_all50_2, aes(x = Year.x, y = percentile)) +
+    geom_point(aes(color = Event, size = 10), alpha = .8) +
+    scale_size_continuous(range = range(c(3,10))) +
+    facet_grid(. ~ filter) +
+    theme_bw() +
+    theme(legend.position="none",
+          plot.title = element_text(face="bold"),
+          axis.text.x = element_text(face = "bold"),
+          axis.title.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"),
+          axis.title.y = element_text(face = "bold"),
+          strip.text.x = element_text(face="bold")) +
+    labs(x = "", y = "Percentile Finish",
+         title = "My Percentile Finish: All Ages vs. Ages 50+")
+dev.off()
+
+############ Unused Code ############
+
+## Distributions of Times
+# All Races Distribution
+ggplot(times, aes(x=hms, fill = as.factor(Distance))) +
+    geom_histogram(binwidth = 30, alpha = 0.5, position = "identity") +
+    theme_classic()
+
+ggplot(times, aes(x=hms, fill = as.factor(Distance))) +
+    geom_density(alpha = .5) +
+    theme_classic()
+
+# Half Marathon Distribution
+filter(times, Distance == 13.1) %>%
+    ggplot(aes(x=hms)) + 
+    geom_histogram()
+
+# Marathon Distribution
+filter(times, Distance == 26.2) %>%
+    ggplot(aes(x=hms)) + 
+    geom_histogram()
+
+filter(times, Distance == 6.2) %>%
+    ggplot(aes(x=hms)) + 
+    geom_histogram() +
+    theme_classic()
+
+# Half Marathon Results
+half <- filter(times, Distance == 13.1)
+
+breaks_half <- scale_color_discrete(name = "Half Marathons",
+                                    breaks = c("4 Bridges", "Athens Half Marathon", "Save the Light", "TomKingClassic"),
+                                    labels = c("4 Bridges",
+                                               "Athens",
+                                               "Save The Light",
+                                               "Tom King"))
+
+half %>%
+    group_by(PLACE, JAY, hms) %>%
+    ggplot(aes(x = hms, y = PLACE)) +
+    geom_point(aes(color=Event, size = JAY)) +
+    guides(size = F) +
+    theme_classic() +
+    theme(axis.text.x = element_text(face = "bold"),
+          axis.title.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"),
+          axis.title.y = element_text(face = "bold")) +
+    labs(x = "Finish Time", y = "Place") +
+    breaks_half
+
+# Median per Event
+group_by(times, Event) %>%
+    summarise(median = median(hms, na.rm=T))
+
+# Plots
 ggplot(jay_all50, aes(x = filter, y = percentile, group = Event)) +
     geom_line(aes(color = Event), linetype = "dashed") +
     geom_point(aes(color = Event, shape = filter, size = 10), alpha = .8) +
@@ -231,3 +204,27 @@ ggplot(jay_all50, aes(x = filter, y = percentile, group = Event)) +
           axis.text.y = element_text(face = "bold"),
           axis.title.y = element_text(face = "bold")) +
     labs(x = "All Ages vs. Ages 50+", y = "Percentile Finish")
+
+ggplot(jay50_pct, aes(x = Year, y = percentile)) +
+    geom_point(aes(color = Event, size = 10), alpha = .8) +
+    theme_classic() +
+    guides(size = F) +
+    breaks_all +
+    scale_size_continuous(range = range(c(3,10))) +
+    theme(axis.text.x = element_text(face = "bold"),
+          axis.title.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"),
+          axis.title.y = element_text(face = "bold")) +
+    labs(x = "Year", y = "Percentile Finish")
+
+ggplot(jay_pct, aes(x = Year, y = percentile)) +
+    geom_point(aes(color = Event, size = 10), alpha = .8) +
+    theme_classic() +
+    guides(size = F) +
+    breaks_all +
+    scale_size_continuous(range = range(c(3,10))) +
+    theme(axis.text.x = element_text(face = "bold"),
+          axis.title.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"),
+          axis.title.y = element_text(face = "bold")) +
+    labs(x = "Year", y = "Percentile Finish")
