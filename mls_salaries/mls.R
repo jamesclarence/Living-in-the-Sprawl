4edbd1ec8fa1d3ffc60720ded3b31b9ad52f5b62
Jay Fisher <fisher.jc@gmail.com>
2015-02-20 22:15:19 -0600

    mls_3
    Final plots and code

--- mls_salaries/mls.R
+++ mls_salaries/mls.R
@@ -15,58 +15,8 @@ mls_var <- select(mls, year, club, lastname, guaranteedcompensation) %>%
                sal_var = ((guaranteedcompensation - sal_mn)/sal_sd))
 
 ### Plots
-# Plot: Distribution of MLS Salaries from 2007-2014 (Standard Deviation)
-ggplot(mls_var, aes(x = sal_var, y = as.factor(year))) +
-    geom_point(color = "dark green", size = 3, alpha = 0.5) +
-    scale_y_discrete(limits = rev(levels(as.factor(mls_var$year)))) +
-    labs(x = "", y = "",
-         title = "Distribution of MLS Salaries from 2007-2014 (Standard Deviation)") +
-    theme_minimal() +
-    theme(plot.title = element_text(face="bold"),
-          axis.text.x = element_text(face = "bold"),
-          axis.title.x = element_text(face = "bold"),
-          axis.text.y = element_text(face = "bold"),
-          axis.title.y = element_text(face = "bold"))
-
-ggplot(mls_var, aes(x = sal_var, y = as.factor(year))) +
-    geom_point(color = "dark green", size = 3, alpha = 0.5) +
-    xlim(-1, 1) +
-    scale_y_discrete(limits = rev(levels(as.factor(mls_var$year)))) +
-    labs(x = "", y = "",
-         title = "Distribution of MLS Salaries from 2007-2014 (Standard Deviation)") +
-    theme_minimal() +
-    theme(plot.title = element_text(face="bold"),
-          axis.text.x = element_text(face = "bold"),
-          axis.title.x = element_text(face = "bold"),
-          axis.text.y = element_text(face = "bold"),
-          axis.title.y = element_text(face = "bold"))
-
-# Plot: Salary Distribution of MLS Players, Histogram (Count - All)
-ggplot(mls_var, aes(x = sal_var)) +
-    geom_histogram(binwidth = 1, color = "black", fill = "dark green") +
-    labs(x = "Standard Deviation", y = "Number of Players",
-         title = "Count of MLS Salaries from 2007-2014") +
-    theme_minimal() +
-    theme(plot.title = element_text(face="bold"),
-          axis.text.x = element_text(face = "bold"),
-          axis.title.x = element_text(face = "bold"),
-          axis.text.y = element_text(face = "bold"),
-          axis.title.y = element_text(face = "bold"))
-
-# Plot: Salary Distribution of MLS Players, Histogram (Count - 1 SD)
-ggplot(mls_var, aes(x = sal_var)) +
-    geom_histogram(binwidth = 0.25, color = "black", fill = "dark green") +
-    xlim(-1, 1) +
-    labs(x = "Standard Deviation", y = "Number of Players",
-         title = "Count of MLS Salaries from 2007-2014") +
-    theme_minimal() +
-    theme(plot.title = element_text(face="bold"),
-          axis.text.x = element_text(face = "bold"),
-          axis.title.x = element_text(face = "bold"),
-          axis.text.y = element_text(face = "bold"),
-          axis.title.y = element_text(face = "bold"))
-
-# Plot: Distribution of MLS Salaries from 2007-2014 ($ Values)
+# Plot 1: Distribution of MLS Salaries from 2007-2014 ($ Values)
+png("plot1.png", height = 300, width = 636)
 ggplot(mls_var, aes(x = guaranteedcompensation, y = as.factor(year))) +
     geom_point(color = "dark green", size = 3, alpha = 0.5) +
     scale_x_log10(breaks = c(10000, 50000, 100000, 250000, 1000000,
@@ -80,8 +30,54 @@ ggplot(mls_var, aes(x = guaranteedcompensation, y = as.factor(year))) +
           axis.title.x = element_text(face = "bold"),
           axis.text.y = element_text(face = "bold"),
           axis.title.y = element_text(face = "bold"))
+dev.off()
 
-# Another Chart: One or two teams (Seattle, Los Angeles, Toronto)
+# Plot 2: Distribution of MLS Salaries from 2007-2014 (Standard Deviation)
+png("plot2.png", height = 300, width = 636)
+ggplot(mls_var, aes(x = sal_var, y = as.factor(year))) +
+    geom_point(color = "dark green", size = 3, alpha = 0.5) +
+    scale_y_discrete(limits = rev(levels(as.factor(mls_var$year)))) +
+    labs(x = "", y = "",
+         title = "Distribution of MLS Salaries from 2007-2014 (Standard Deviation)") +
+    theme_minimal() +
+    theme(plot.title = element_text(face="bold"),
+          axis.text.x = element_text(face = "bold"),
+          axis.title.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"),
+          axis.title.y = element_text(face = "bold"))
+dev.off()
+
+# Plot 3: Salary Distribution of MLS Players, Histogram (Count - All)
+png("plot3.png", height = 300, width = 636)
+ggplot(mls_var, aes(x = sal_var)) +
+    geom_histogram(binwidth = 1, color = "black", fill = "dark green") +
+    labs(x = "Standard Deviation", y = "Number of Players",
+         title = "Count of MLS Salaries from 2007-2014") +
+    theme_minimal() +
+    theme(plot.title = element_text(face="bold"),
+          axis.text.x = element_text(face = "bold"),
+          axis.title.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"),
+          axis.title.y = element_text(face = "bold", vjust = 1))
+dev.off()
+
+# Plot 4: Salary Distribution of MLS Players, Histogram (Count - 1 SD)
+png("plot4.png", height = 300, width = 636)
+ggplot(mls_var, aes(x = sal_var)) +
+    geom_histogram(binwidth = 0.25, color = "black", fill = "dark green") +
+    xlim(-1, 1) +
+    labs(x = "Standard Deviation", y = "Number of Players",
+         title = "Count of MLS Salaries from 2007-2014, One Standard Deviation") +
+    theme_minimal() +
+    theme(plot.title = element_text(face="bold"),
+          axis.text.x = element_text(face = "bold"),
+          axis.title.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"),
+          axis.title.y = element_text(face = "bold", vjust = 1))
+dev.off()
+
+# Plot 5: Salary Distribution of LA Galaxy, 2007-2014
+png("plot5.png", height = 300, width = 636)
 filter(mls, club == "LA") %>%
     ggplot(aes(x = guaranteedcompensation, y = as.factor(year))) +
     geom_point(color = "dark blue", size = 3, alpha = 0.75) +
@@ -96,10 +92,26 @@ filter(mls, club == "LA") %>%
           axis.title.x = element_text(face = "bold"),
           axis.text.y = element_text(face = "bold"),
           axis.title.y = element_text(face = "bold"))
+dev.off()
 
+### Stats
 # Median and Average Salary from 2007-2014
 summarise(mls_var, median = median(guaranteedcompensation, na.rm = T),
           mean = mean(guaranteedcompensation, na.rm = T))
 
 # Players making over a million, 2007-2014
 filter(mls_var, guaranteedcompensation >= 1000000) %>% summarise(count = n())
+
+### Unused Plots
+ggplot(mls_var, aes(x = sal_var, y = as.factor(year))) +
+    geom_point(color = "dark green", size = 3, alpha = 0.5) +
+    xlim(-1, 1) +
+    scale_y_discrete(limits = rev(levels(as.factor(mls_var$year)))) +
+    labs(x = "", y = "",
+         title = "Distribution of MLS Salaries from 2007-2014 (Standard Deviation)") +
+    theme_minimal() +
+    theme(plot.title = element_text(face="bold"),
+          axis.text.x = element_text(face = "bold"),
+          axis.title.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"),
+          axis.title.y = element_text(face = "bold"))
